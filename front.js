0f74f69a78648857dd8322612ed4b4322e37f13e
Angelo Miguel Arcilla <miguel.arcilla@live.com>
2018-11-25 14:44:34 +0800

    reversed electric boogaloo

--- front.js
+++ front.js
@@ -5,10 +5,6 @@ require('bootstrap-datepicker/dist/css/bootstrap-datepicker3.standalone.css');
 require('./scss/main.scss');
 
 $(document).ready(function(){
-    setTimeout(function() { 
-        alert('Shoutout to DevCon Davao!'); 
-    }, 3000); 
-
     var today = $('.datepicker').data('start') || new Date();
     var options = {
         format: 'yyyy-mm-dd',
